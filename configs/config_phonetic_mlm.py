de1745cc8d554e1d56915e434e1c934b1a996b7d
GitYCC <ycc.tw.email@gmail.com>
2021-11-16 09:18:08 +0800

    [feature] Revise alpha

--- configs/config_phonetic_mlm.py
+++ configs/config_phonetic_mlm.py
@@ -4,4 +4,4 @@ batch_size = 32
 detector_config_path = 'configs/config_detect.py'
 detector_checkpoint_path = 'saved_models/bert_detection/best_f1.pth'
 detector_threshold = 0.5
-alpha = 500
+alpha = 7
